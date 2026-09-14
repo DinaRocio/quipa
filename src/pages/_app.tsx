5f569370e269a68f4a0af18c2aec55054cda0a48
Dina Rocio <dinarociovq15@hotmail.com>
2026-09-14 16:40:45 -0500

    feat: integrate Vercel Analytics for page tracking

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,14 @@
 import GlobalStyles from "@/styles/globals";
 
 import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <GlobalStyles />
       <Component {...pageProps} />
+      <Analytics />
     </>
   );
 }
